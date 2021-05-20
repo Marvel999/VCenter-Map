b85900c0cc2ba473cd30c23242daa74b69b5f08d
Marvel999 <manishjobs1999@gmail.com>
2021-05-20 11:59:40 +0530

    Add Firebase google singin

--- app/src/main/java/com/WrapX/vcentremap/ui/login/LoginFragment.kt
+++ app/src/main/java/com/WrapX/vcentremap/ui/login/LoginFragment.kt
@@ -1,21 +1,37 @@
 package com.WrapX.vcentremap.ui.login
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.WrapX.vcentremap.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class LoginFragment : Fragment() {
 
 
     private lateinit var viewModel: LoginViewModel
     private lateinit var singIn: Button
+    private lateinit var mAuth: FirebaseAuth
 
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN=111;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,10 +46,70 @@ class LoginFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        mAuth = Firebase.auth
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
 
+        mGoogleSignInClient = GoogleSignIn.getClient(this.requireActivity(), gso)
         singIn.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_pinCodeFragment)
+           signIn()
         }
     }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth.currentUser
+//        updateUI(currentUser)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d("Login", "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("Login", "Google sign in failed", e)
+            }
+        }
+    }
+
+
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this.requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("Login", "signInWithCredential:success")
+                    val user = mAuth.currentUser
+                    findNavController().navigate(R.id.action_loginFragment_to_pinCodeFragment)
+//                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("Login", "signInWithCredential:failure", task.exception)
+//                    updateUI(null)
+                }
+            }
+    }
+
+
 
 }
