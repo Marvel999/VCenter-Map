f00facf43c38fe6367c066d02721fc0b37b2b9a1
Marvel999 <manishjobs1999@gmail.com>
2021-05-28 17:41:28 +0530

    Add content on info page

--- app/src/main/java/com/WrapX/vcentremap/InfoActivity.kt
+++ app/src/main/java/com/WrapX/vcentremap/InfoActivity.kt
@@ -2,6 +2,7 @@ package com.WrapX.vcentremap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -9,15 +10,31 @@ class InfoActivity : AppCompatActivity() {
 
     private lateinit var backBtn:ImageView
     private lateinit var title:TextView
+    private lateinit var telegram:TextView
+    private lateinit var website:TextView
+    private lateinit var paytmFinder:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
         backBtn=findViewById(R.id.back_btn)
         title=findViewById(R.id.title)
+        website=findViewById(R.id.website)
+        paytmFinder=findViewById(R.id.ptmfinder)
+        telegram=findViewById(R.id.telegramGroup)
         title.text="Tips & Tricks"
+        setClickableText()
         backBtn.setOnClickListener {
             onBackPressed()
         }
+
+
+
+    }
+
+    fun setClickableText(){
+        website.setMovementMethod(LinkMovementMethod.getInstance());
+        paytmFinder.setMovementMethod(LinkMovementMethod.getInstance());
+        telegram.setMovementMethod(LinkMovementMethod.getInstance());
 
     }
 }
